295765ff576e18e56f4fae0f997fc794ebf227ab
Tony Morris <tmorris@tmorris.net>
2017-09-01 14:23:38 +1000

    fix nix

--- aip.nix
+++ aip.nix
@@ -1,21 +1,27 @@
 { mkDerivation, base, bytestring, digit, directory, doctest
-, filepath, HTTP, network-uri, papa, parsec, parsers, QuickCheck
-, quickcheck-text, stdenv, tagsoup, tagsoup-selection
+, filepath, HTTP, lens, network-uri, papa, parsec, parsers
+, QuickCheck, quickcheck-text, stdenv, tagsoup, tagsoup-selection
 , template-haskell, transformers
 }:
 mkDerivation {
   pname = "aip";
   version = "0.0.1";
   src = ./.;
+  isLibrary = true;
+  isExecutable = true;
   libraryHaskellDepends = [
-    base bytestring digit directory filepath HTTP network-uri papa
+    base bytestring digit directory filepath HTTP lens network-uri papa
+    parsec parsers tagsoup tagsoup-selection transformers
+  ];
+  executableHaskellDepends = [
+    base bytestring digit directory filepath HTTP lens network-uri papa
     parsec parsers tagsoup tagsoup-selection transformers
   ];
   testHaskellDepends = [
     base directory doctest filepath parsec QuickCheck quickcheck-text
     template-haskell
   ];
-  homepage = "https://github.com/tonymorris/aip";
+  homepage = "https://github.com/qfpl/aip";
   description = "Aeronautical Information Package (AIP)";
   license = stdenv.lib.licenses.bsd3;
 }
