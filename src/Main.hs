d6d596c5de394b437fad0b138da3dd57b5142066
Tony Morris <tmorris@tmorris.net>
2017-11-09 15:01:20 +1000

    latest dir, with .tar.gz

--- src/Main.hs
+++ src/Main.hs
@@ -5,8 +5,11 @@ module Main(
 ) where
 
 import Data.Aviation.Aip.AipDocuments(distributeAipDocuments)
+import Data.Time(UTCTime(utctDay, utctDayTime), TimeOfDay(TimeOfDay), toGregorian, timeToTimeOfDay, getCurrentTime)
 import System.Environment(getArgs)
 import System.IO(IO, hPutStrLn, stderr)
+import Sys.Exit(CreateProcess, procIn, createMakeWaitProcessM, exit)
+import System.FilePath((</>))
 import Papa
 
 main ::
@@ -14,7 +17,48 @@ main ::
 main =
   do  a <- getArgs
       case a of
-        adir:ldir:_ ->
-          void (distributeAipDocuments adir ldir)
+        adir:_ ->
+          do  t <- getCurrentTime
+              let u = time t
+                  d = adir </> u ++ "UTC"
+              void (distributeAipDocuments (d </> "aip") (d </> "log"))
+              exit $ do   createMakeWaitProcessM . linkLatest adir $ u
+                          createMakeWaitProcessM . tarAip $ d
         _ ->
-          hPutStrLn stderr "<aip-directory> <log-directory>"
+          hPutStrLn stderr "<aip-output-directory>"
+
+tarAip ::
+  FilePath
+  -> CreateProcess
+tarAip d =
+  procIn d "tar"
+    [
+      "-zcvf"
+    , "aip.tar.gz"
+    , "aip"
+    ]
+
+linkLatest ::
+  FilePath
+  -> String
+  -> CreateProcess
+linkLatest d t =
+  procIn d "ln"
+    [
+      "-f"
+    , "-s"
+    , "-n"
+    , t
+    , "latest"
+    ]
+
+time ::
+  UTCTime
+  -> String
+time t =
+  let show2 = let s2 [x] = ['0', x]
+                  s2 x = x
+              in s2 . show
+      (y, m, d) = toGregorian (utctDay t)
+      TimeOfDay h n s = timeToTimeOfDay (utctDayTime t)
+  in concat [show y, show2 m, show2 d, "-", show2 h, show2 n, show2 (floor s)]
