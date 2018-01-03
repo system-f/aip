dd434494c6863a44c24c43f23ebffe6187286652
Tony Morris <tmorris@tmorris.net>
2018-01-03 11:58:02 +1000

    ci

---
+++ ci/jobsets.nix
@@ -0,0 +1,27 @@
+{ nixpkgs, declInput }: let pkgs = import nixpkgs {}; in {
+  jobsets = pkgs.runCommand "spec.json" {} ''
+    cat <<EOF
+    ${builtins.toXML declInput}
+    EOF
+    cat > $out <<EOF
+    {
+        "aip": {
+            "enabled": 1,
+            "hidden": false,
+            "description": "aip",
+            "nixexprinput": "aip",
+            "nixexprpath": "ci/ci.nix",
+            "checkinterval": 300,
+            "schedulingshares": 1,
+            "enableemail": false,
+            "emailoverride": "",
+            "keepnr": 5,
+            "inputs": {
+                "aip": { "type": "git", "value": "https://github.com/qfpl/aip", "emailresponsible": false },
+                "nixpkgs": { "type": "git", "value": "https://github.com/NixOS/nixpkgs.git release-17.03", "emailresponsible": false }
+            }
+        }
+    }
+    EOF
+  '';
+}
