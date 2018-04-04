491f4d25c4e3b1774bc2d5e550938acee4ef4216
Tony Morris <tmorris@tmorris.net>
2018-04-04 10:43:23 +1000

    update to simple build

--- default.nix
+++ default.nix
@@ -15,10 +15,19 @@ let
       rev = "9752ec3341df8117121c23e2fa8eadc38af7841b";
       sha256 = "05g00hjd8hi5lvc9k9smqh6s4sjyd07hr94qicjsigl5k241gibh";
     };
+
+    digit = pkgs.fetchFromGitHub {
+      owner = "qfpl";
+      repo = "digit";
+      rev = "df2cf23cbd74c07809a394c52c3decb51dd06438";
+      sha256 = "0hsgr2xngvdaqi5n8sg080ifl64b94xm2mw87jwnx55i7gq1mkic";
+    };
+
   };
 
   modifiedHaskellPackages = haskellPackages.override {
     overrides = self: super: import sources.papa self // {
+      digit = import sources.digit { inherit nixpkgs compiler; };
       parsers = pkgs.haskell.lib.dontCheck super.parsers;
       tagsoup-selection = pkgs.haskell.lib.doJailbreak super.tagsoup-selection;
     };
