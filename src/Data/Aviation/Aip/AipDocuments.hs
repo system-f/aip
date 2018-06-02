c8cc562ceb09bc53892fb254e7d44a698c55a114
Tony Morris <tmorris@tmorris.net>
2018-06-02 15:53:37 +1000

    fix urls

--- src/Data/Aviation/Aip/AipDocuments.hs
+++ src/Data/Aviation/Aip/AipDocuments.hs
@@ -1238,7 +1238,7 @@ aipDocuments (Ersas ersas) =
         , req rds                     (\f q s -> q ++ "ersa/" ++ f ++ s ++ ".pdf")
         , req charts                  (\f q s -> q ++ f ++ s ++ ".pdf")
         , req books                   (\f q s -> q ++ f ++ s ++ ".pdf")
-        , req dap                     (\f _ _ -> f ++ ".pdf")
-        , req dah                     (\f _ _ -> f ++ ".pdf")
-        , req precisionApproachCharts (\f _ _ -> f ++ ".pdf")
+        , req dap                     (\f _ _ -> f)
+        , req dah                     (\f _ _ -> f)
+        , req precisionApproachCharts (\f _ _ -> f)
         ]
