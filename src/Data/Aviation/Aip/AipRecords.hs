f058417e2c3044d45e3a1f6ca6e9a02baac1e51a
Tony Morris <tmorris@tmorris.net>
2018-08-26 15:36:10 +1000

    modules

--- src/Data/Aviation/Aip/AipRecords.hs
+++ src/Data/Aviation/Aip/AipRecords.hs
@@ -3,12 +3,29 @@
 
 module Data.Aviation.Aip.AipRecords(
   AipRecords(..)
+, getAipRecords
 ) where
 
+import Codec.Binary.UTF8.String as UTF8(encode)
+import Data.Aeson(decodeFileStrict)
+import Data.Aeson.Encode.Pretty(confIndent, defConfig, Indent(Spaces), encodePretty')
+import qualified Data.ByteString.Lazy as LazyByteString(writeFile)
+import Data.Time(getCurrentTime)
 import Data.Aeson(FromJSON(parseJSON), ToJSON(toJSON), withObject, object, (.:), (.=))
-import Data.Aviation.Aip.AipRecord(AipRecord, ManyAipRecord(_ManyAipRecord))
-import Data.Aviation.Aip.SHA1(SHA1)
+import Data.Aviation.Aip.AipDocument(AipDocument(Aip_Book, Aip_Charts, Aip_SUP_AIC, Aip_DAP, Aip_DAH, Aip_ERSA, Aip_AandB_Charts, Aip_Summary_SUP_AIC), runAipDocument)
+import Data.Aviation.Aip.AipDocuments(AipDocuments1, AipDocuments(AipDocuments))
+import Data.Aviation.Aip.AipRecord(AipRecord(AipRecord), ManyAipRecord(_ManyAipRecord))
+import Data.Aviation.Aip.Cache(Cache, isReadOrWriteCache, isWriteCache)
+import Data.Aviation.Aip.ConnErrorHttp4xx(AipConn)
+import Data.Aviation.Aip.HttpRequest(requestAipContents)
+import Data.Aviation.Aip.SHA1(SHA1, hash, hashHex)
+import Control.Monad.IO.Class(liftIO)
 import Papa hiding ((.=))
+import System.Directory(doesFileExist, getPermissions, readable, createDirectoryIfMissing)
+import System.FilePath(takeDirectory, (</>))
+import Text.HTML.TagSoup(Tag(TagText))
+import Text.HTML.TagSoup.Tree(TagTree(TagBranch, TagLeaf), parseTree)
+import Text.HTML.TagSoup.Tree.Zipper(TagTreePos(TagTreePos), fromTagTree, traverseTree)
 
 data AipRecords =
   AipRecords
@@ -30,3 +47,78 @@ instance ToJSON AipRecords where
 instance ManyAipRecord AipRecords where
   _ManyAipRecord f (AipRecords s r) =
     AipRecords s <$> traverse f r
+
+getAipRecords ::
+  Cache
+  -> FilePath -- basedir
+  -> AipConn AipRecords
+getAipRecords cch dir =
+  let readCache ::
+        FilePath
+        -> IO (Maybe AipRecords)
+      readCache c =
+        if isReadOrWriteCache cch
+          then
+            do  e <- doesFileExist c
+                if e
+                  then
+                    do  p <- getPermissions c
+                        if readable p
+                          then
+                            decodeFileStrict c :: IO (Maybe (AipRecords))
+                          else
+                            pure Nothing
+                  else
+                    pure Nothing
+          else
+            pure Nothing
+
+      writeCache z rs =
+        when (isWriteCache cch) $
+          do  createDirectoryIfMissing True (takeDirectory z)
+              let conf = defConfig { confIndent = Spaces 2 }
+              LazyByteString.writeFile z (encodePretty' conf rs)
+  in  do  c <- requestAipContents
+          let h = hash (UTF8.encode c)
+          let z = dir </> hashHex h ".json"
+          r <- liftIO $ readCache z
+          case r of
+            Just v ->
+              pure v
+            Nothing ->
+              let traverseAipDocuments ::
+                    TagTreePos String
+                    -> AipDocuments1
+                  traverseAipDocuments (TagTreePos (TagBranch "ul" [] x) _ _ _) =
+                    let li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText "AIP Book")], TagLeaf (TagText tx)]) =
+                          [Aip_Book href tx ()]
+                        li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText "AIP Charts")], TagLeaf (TagText tx)]) =
+                          [Aip_Charts href tx ()]
+                        li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText "AIP Supplements and Aeronautical  Information Circulars (AIC)")]]) =
+                          [Aip_SUP_AIC href ()]
+                        li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText "Departure and Approach Procedures (DAP)")], TagLeaf (TagText tx)]) =
+                          [Aip_DAP href tx ()]
+                        li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText "Designated Airspace Handbook (DAH)")], TagLeaf (TagText tx)]) =
+                          [Aip_DAH href tx]
+                        li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText "En Route Supplement Australia (ERSA)")], TagLeaf (TagText tx)]) =
+                          [Aip_ERSA href tx ()]
+                        li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText "Precision Approach Terrain Charts and Type A & Type B Obstacle Charts")]]) =
+                          [Aip_AandB_Charts href]
+                        li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText tx)]]) =
+                          let str = "Summary of SUP/AIC Current"
+                              (p, s) = splitAt (length str) tx
+                          in  if p == str then
+                                [Aip_Summary_SUP_AIC href s]
+                              else
+                                [] 
+                        li _ =
+                          []
+                    in  AipDocuments (x >>= li)
+                  traverseAipDocuments _ =
+                    mempty
+              in  do  let AipDocuments a = foldMap (traverseTree traverseAipDocuments . fromTagTree) (parseTree c)
+                      q <- AipDocuments <$> traverse runAipDocument a
+                      t <- liftIO getCurrentTime
+                      let rs = AipRecords h (AipRecord t q :| [])
+                      liftIO $ writeCache z rs
+                      pure rs
--- src/Main.hs
+++ src/Main.hs
@@ -4,102 +4,13 @@ module Main(
   main
 ) where
 
-import Control.Monad.IO.Class
 import Control.Monad.Trans.Except
-import Data.Aviation.Aip.ConnErrorHttp4xx
-import Data.Aviation.Aip.HttpRequest
-import Data.Time
-import Papa hiding ((.=))
-import System.Directory
-import System.FilePath
-import System.IO
-import Text.HTML.TagSoup
-import Text.HTML.TagSoup.Tree
-import Text.HTML.TagSoup.Tree.Zipper
-import Codec.Binary.UTF8.String
-import Data.Aeson
-import Data.Aeson.Encode.Pretty
-import qualified Data.ByteString.Lazy as LazyByteString
+import System.IO(print)
 import Data.Aviation.Aip
-
-traverseAipDocuments ::
-  TagTreePos String
-  -> AipDocuments1
-traverseAipDocuments (TagTreePos (TagBranch "ul" [] x) _ _ _) =
-  let li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText "AIP Book")], TagLeaf (TagText tx)]) =
-        [Aip_Book href tx ()]
-      li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText "AIP Charts")], TagLeaf (TagText tx)]) =
-        [Aip_Charts href tx ()]
-      li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText "AIP Supplements and Aeronautical  Information Circulars (AIC)")]]) =
-        [Aip_SUP_AIC href ()]
-      li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText "Departure and Approach Procedures (DAP)")], TagLeaf (TagText tx)]) =
-        [Aip_DAP href tx ()]
-      li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText "Designated Airspace Handbook (DAH)")], TagLeaf (TagText tx)]) =
-        [Aip_DAH href tx]
-      li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText "En Route Supplement Australia (ERSA)")], TagLeaf (TagText tx)]) =
-        [Aip_ERSA href tx ()]
-      li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText "Precision Approach Terrain Charts and Type A & Type B Obstacle Charts")]]) =
-        [Aip_AandB_Charts href]
-      li (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText tx)]]) =
-        let str = "Summary of SUP/AIC Current"
-            (p, s) = splitAt (length str) tx
-        in  if p == str then
-              [Aip_Summary_SUP_AIC href s]
-            else
-              [] 
-      li _ =
-        []
-  in  AipDocuments (x >>= li)
-traverseAipDocuments _ =
-  mempty
-
-runX ::
-  Cache
-  -> FilePath -- basedir
-  -> AipConn AipRecords
-runX cch dir =
-  let readCache ::
-        FilePath
-        -> IO (Maybe AipRecords)
-      readCache c =
-        if isReadOrWriteCache cch
-          then
-            do  e <- doesFileExist c
-                if e
-                  then
-                    do  p <- getPermissions c
-                        if readable p
-                          then
-                            decodeFileStrict c :: IO (Maybe (AipRecords))
-                          else
-                            pure Nothing
-                  else
-                    pure Nothing
-          else
-            pure Nothing
-
-      writeCache z rs =
-        when (isWriteCache cch) $
-          do  createDirectoryIfMissing True (takeDirectory z)
-              let conf = defConfig { confIndent = Spaces 2 }
-              LazyByteString.writeFile z (encodePretty' conf rs)
-  in  do  c <- requestAipContents
-          let h = hash (Codec.Binary.UTF8.String.encode c)
-          let z = dir </> hashHex h ".json"
-          r <- liftIO $ readCache z
-          case r of
-            Just v ->
-              pure v
-            Nothing ->
-              do  let AipDocuments a = foldMap (traverseTree traverseAipDocuments . fromTagTree) (parseTree c)
-                  q <- AipDocuments <$> traverse runAipDocument a
-                  t <- liftIO getCurrentTime
-                  let rs = AipRecords h (AipRecord t q :| [])
-                  liftIO $ writeCache z rs
-                  pure rs
+import Papa hiding ((.=))
 
 main ::
   IO ()
 main =
-  do  x <- runExceptT $ runX ReadWriteCache "/tmp/abc"
+  do  x <- runExceptT $ getAipRecords ReadWriteCache "/tmp/abc"
       print x
