ec3e5aae59547d6d4c266cf8b5bc2953f604d96c
Tony Morris <tmorris@tmorris.net>
2018-08-28 09:57:30 +1000

    lens

--- src/Data/Aviation/Aip/AipDate.hs
+++ src/Data/Aviation/Aip/AipDate.hs
@@ -5,6 +5,13 @@
 
 module Data.Aviation.Aip.AipDate(
   AipDate(..)
+, AsAipDate(..)
+, FoldAipDate(..)
+, GetAipDate(..)
+, SetAipDate(..)
+, ManyAipDate(..)
+, HasAipDate(..)
+, IsAipDate(..)
 ) where
 
 import Data.Aeson(FromJSON(parseJSON), ToJSON(toJSON))
@@ -42,3 +49,87 @@ instance Wrapped AipDate where
 
 instance AipDate ~ a =>
   Rewrapped AipDate a
+
+class AsAipDate a where
+  _AipDate ::
+    Prism' a AipDate
+    
+instance AsAipDate AipDate where
+  _AipDate =
+    id
+  
+instance AsAipDate String where
+  _AipDate =
+    from _Wrapped
+
+class FoldAipDate a where
+  _FoldAipDate ::
+    Fold a AipDate
+    
+instance FoldAipDate AipDate where
+  _FoldAipDate =
+    id
+
+instance FoldAipDate String where
+  _FoldAipDate =
+    from _Wrapped
+
+class FoldAipDate a => GetAipDate a where
+  _GetAipDate ::
+    Getter a AipDate
+    
+instance GetAipDate AipDate where
+  _GetAipDate =
+    id
+
+instance GetAipDate String where
+  _GetAipDate =
+    from _Wrapped
+
+class SetAipDate a where
+  _SetAipDate ::
+    Setter' a AipDate
+    
+instance SetAipDate AipDate where
+  _SetAipDate =
+    id
+
+instance SetAipDate String where
+  _SetAipDate =
+    from _Wrapped
+
+class (FoldAipDate a, SetAipDate a) => ManyAipDate a where
+  _ManyAipDate ::
+    Traversal' a AipDate
+
+instance ManyAipDate AipDate where
+  _ManyAipDate =
+    id
+
+instance ManyAipDate String where
+  _ManyAipDate =
+    from _Wrapped
+
+class (GetAipDate a, ManyAipDate a) => HasAipDate a where
+  aipDate ::
+    Lens' a AipDate
+
+instance HasAipDate AipDate where
+  aipDate =
+    id
+
+instance HasAipDate String where
+  aipDate =
+    from _Wrapped
+
+class (HasAipDate a, AsAipDate a) => IsAipDate a where
+  _IsAipDate ::
+    Iso' a AipDate
+    
+instance IsAipDate AipDate where
+  _IsAipDate =
+    id
+
+instance IsAipDate String where
+  _IsAipDate =
+    from _Wrapped
