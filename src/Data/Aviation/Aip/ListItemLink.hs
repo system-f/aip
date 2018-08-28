ff1ac8ba87f11a5415ea9e99c1d1026a0d77eb89
Tony Morris <tmorris@tmorris.net>
2018-08-28 20:29:57 +1000

    lens

--- src/Data/Aviation/Aip/ListItemLink.hs
+++ src/Data/Aviation/Aip/ListItemLink.hs
@@ -1,9 +1,16 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DefaultSignatures #-}
 
 module Data.Aviation.Aip.ListItemLink(
   ListItemLink(..)
+, AsListItemLink(..)
+, FoldListItemLink(..)
+, GetListItemLink(..)
+, SetListItemLink(..)
 , ManyListItemLink(..)
+, HasListItemLink(..)
+, IsListItemLink(..)
 ) where
 
 import Data.Aviation.Aip.Href(Href)
@@ -17,14 +24,6 @@ data ListItemLink =
     Txt
   deriving (Eq, Ord, Show)
 
-class ManyListItemLink a where
-  _ManyListItemLink ::
-    Traversal' a ListItemLink
-
-instance ManyListItemLink ListItemLink where
-  _ManyListItemLink =
-    id
-
 instance FromJSON ListItemLink where
   parseJSON =
     withObject "ListItemLink" $ \v ->
@@ -35,3 +34,79 @@ instance FromJSON ListItemLink where
 instance ToJSON ListItemLink where
   toJSON (ListItemLink u t) =
     object ["href" .= u, "txt" .= t]
+
+class AsListItemLink a where
+  _ListItemLink ::
+    Prism' a ListItemLink
+  default _ListItemLink ::
+    IsListItemLink a =>
+    Prism' a ListItemLink
+  _ListItemLink =
+    _IsListItemLink
+    
+instance AsListItemLink ListItemLink where
+  _ListItemLink =
+    id
+
+class FoldListItemLink a where
+  _FoldListItemLink ::
+    Fold a ListItemLink
+    
+instance FoldListItemLink ListItemLink where
+  _FoldListItemLink =
+    id
+
+class FoldListItemLink a => GetListItemLink a where
+  _GetListItemLink ::
+    Getter a ListItemLink
+  default _GetListItemLink ::
+    HasListItemLink a =>
+    Getter a ListItemLink
+  _GetListItemLink =
+    listItemLink
+    
+instance GetListItemLink ListItemLink where
+  _GetListItemLink =
+    id
+
+class SetListItemLink a where
+  _SetListItemLink ::
+    Setter' a ListItemLink
+  default _SetListItemLink ::
+    ManyListItemLink a =>
+    Setter' a ListItemLink
+  _SetListItemLink =
+    _ManyListItemLink
+
+instance SetListItemLink ListItemLink where
+  _SetListItemLink =
+    id
+
+class (FoldListItemLink a, SetListItemLink a) => ManyListItemLink a where
+  _ManyListItemLink ::
+    Traversal' a ListItemLink
+
+instance ManyListItemLink ListItemLink where
+  _ManyListItemLink =
+    id
+
+class (GetListItemLink a, ManyListItemLink a) => HasListItemLink a where
+  listItemLink ::
+    Lens' a ListItemLink
+  default listItemLink ::
+    IsListItemLink a =>
+    Lens' a ListItemLink
+  listItemLink =
+    _IsListItemLink
+
+instance HasListItemLink ListItemLink where
+  listItemLink =
+    id
+
+class (HasListItemLink a, AsListItemLink a) => IsListItemLink a where
+  _IsListItemLink ::
+    Iso' a ListItemLink
+    
+instance IsListItemLink ListItemLink where
+  _IsListItemLink =
+    id
--- src/Data/Aviation/Aip/ListItemLinks.hs
+++ src/Data/Aviation/Aip/ListItemLinks.hs
@@ -3,13 +3,21 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DefaultSignatures #-}
 
 module Data.Aviation.Aip.ListItemLinks(
   ListItemLinks(..)
+, AsListItemLinks(..)
+, FoldListItemLinks(..)
+, GetListItemLinks(..)
+, SetListItemLinks(..)
+, ManyListItemLinks(..)
+, HasListItemLinks(..)
+, IsListItemLinks(..)
 ) where
 
 import Data.Aeson(FromJSON(parseJSON), ToJSON(toJSON), withArray)
-import Data.Aviation.Aip.ListItemLink(ListItemLink, ManyListItemLink(_ManyListItemLink))
+import Data.Aviation.Aip.ListItemLink(ListItemLink, ManyListItemLink(_ManyListItemLink), SetListItemLink, FoldListItemLink(_FoldListItemLink))
 import Papa
 
 newtype ListItemLinks =
@@ -27,17 +35,14 @@ instance Monoid ListItemLinks where
   mempty =
     ListItemLinks []
 
-instance ListItemLinks ~ x => Rewrapped ListItemLinks x
-
 instance Wrapped ListItemLinks where
   type Unwrapped ListItemLinks =
     [ListItemLink]
   _Wrapped' =
     iso (\(ListItemLinks x) -> x) ListItemLinks
 
-instance ManyListItemLink ListItemLinks where
-  _ManyListItemLink f (ListItemLinks x) =
-    ListItemLinks <$> traverse f x
+instance ListItemLinks ~ x =>
+  Rewrapped ListItemLinks x
 
 instance FromJSON ListItemLinks where
   parseJSON =
@@ -47,3 +52,91 @@ instance FromJSON ListItemLinks where
 instance ToJSON ListItemLinks where
   toJSON (ListItemLinks x) =
     toJSON x
+
+class AsListItemLinks a where
+  _ListItemLinks ::
+    Prism' a ListItemLinks
+  default _ListItemLinks ::
+    IsListItemLinks a =>
+    Prism' a ListItemLinks
+  _ListItemLinks =
+    _IsListItemLinks
+    
+instance AsListItemLinks ListItemLinks where
+  _ListItemLinks =
+    id
+
+class FoldListItemLinks a where
+  _FoldListItemLinks ::
+    Fold a ListItemLinks
+    
+instance FoldListItemLinks ListItemLinks where
+  _FoldListItemLinks =
+    id
+
+class FoldListItemLinks a => GetListItemLinks a where
+  _GetListItemLinks ::
+    Getter a ListItemLinks
+  default _GetListItemLinks ::
+    HasListItemLinks a =>
+    Getter a ListItemLinks
+  _GetListItemLinks =
+    listItemLinks
+    
+instance GetListItemLinks ListItemLinks where
+  _GetListItemLinks =
+    id
+
+class SetListItemLinks a where
+  _SetListItemLinks ::
+    Setter' a ListItemLinks
+  default _SetListItemLinks ::
+    ManyListItemLinks a =>
+    Setter' a ListItemLinks
+  _SetListItemLinks =
+    _ManyListItemLinks
+
+instance SetListItemLinks ListItemLinks where
+  _SetListItemLinks =
+    id
+
+class (FoldListItemLinks a, SetListItemLinks a) => ManyListItemLinks a where
+  _ManyListItemLinks ::
+    Traversal' a ListItemLinks
+
+instance ManyListItemLinks ListItemLinks where
+  _ManyListItemLinks =
+    id
+
+class (GetListItemLinks a, ManyListItemLinks a) => HasListItemLinks a where
+  listItemLinks ::
+    Lens' a ListItemLinks
+  default listItemLinks ::
+    IsListItemLinks a =>
+    Lens' a ListItemLinks
+  listItemLinks =
+    _IsListItemLinks
+
+instance HasListItemLinks ListItemLinks where
+  listItemLinks =
+    id
+
+class (HasListItemLinks a, AsListItemLinks a) => IsListItemLinks a where
+  _IsListItemLinks ::
+    Iso' a ListItemLinks
+    
+instance IsListItemLinks ListItemLinks where
+  _IsListItemLinks =
+    id
+
+----
+
+instance SetListItemLink ListItemLinks where
+
+instance FoldListItemLink ListItemLinks where
+  _FoldListItemLink =
+    _ManyListItemLink
+
+instance ManyListItemLink ListItemLinks where
+  _ManyListItemLink f (ListItemLinks x) =
+    ListItemLinks <$> traverse f x
