2239620cc65643786642c15b3b23a65415474c0a
Tony Morris <tmorris@tmorris.net>
2018-08-31 16:40:41 +1000

    dnld

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -10,20 +10,40 @@ import qualified Data.ByteString.Lazy as LazyByteString(ByteString, writeFile)
 import System.IO(print)
 import Data.Aviation.Aip
 import Papa hiding ((.=))
+import System.Directory
+import System.FilePath
 
 main ::
   IO ()
 main =
   do  e <-  runExceptT $ 
               do  x <- getAipRecords ReadWriteCache "/tmp/abc"
-                  mapMOf_ _ManyHref downloadHref x
+                  mapMOf_ _ManyHref (\k -> liftIO (print k) *> downloadHref k) x
       print e
-    
+   
+basedir =
+  "/tmp/def"
+ 
 downloadHref hf =
   do  r <- doGetRequest hf "" :: ExceptT ConnErrorHttp4xx IO LazyByteString.ByteString
-      let w = r
-      liftIO $ print hf
+      let (j, k) = splitFileName (hf ^. _Wrapped)
+      let o = basedir </> j
+      liftIO $ createDirectoryIfMissing True o
+      liftIO $ LazyByteString.writeFile (o </> k) r
 
+{-
+Href "aip.asp?pg=20&vdate=16AUG2018&ver=1"
+Href "current/aip/complete_16AUG2018.pdf"
+Href "current/aip/general_16AUG2018.pdf"
+Href "current/aip/enroute_16AUG2018.pdf"
+Href "current/aip/aerodrome_16AUG2018.pdf"
+Href "current/aip/cover_16AUG2018.pdf"
+Href "aip.asp?pg=60&vdate=24-MAY-2018&ver=1"
+Href "aip.asp?pg=60&vdate=24-MAY-2018&sect=ERCHigh&ver=1"
+Href "/aip/current/aipchart/erch/erch1_24MAY2018.pdf"
+-}
+
+undefined = undefined
 {- todo
 
 * library support
