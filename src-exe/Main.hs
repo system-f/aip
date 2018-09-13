4a2cb26f3a2545290b9678654b3cb5c95831bbb6
Tony Morris <tmorris@tmorris.net>
2018-09-13 13:35:30 +1000

    aeson

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -11,19 +11,3 @@ main ::
   IO ()
 main =
   run nothingAfterDownload
-
-{- todo
-
-* write README of general flow
-* test on windows
-
-other lib
-* tar download
-* move symlinks to their own directory
-* crop pdf files with pdfcrop --margins
-* convert pdf to ps with pdftops
-
-http://classic.austlii.edu.au/au/legis/cth/consol_reg/casr1998333/s175.145.html
-http://www.airservicesaustralia.com /services/aeronautical-information-and-management-services/electronic-data/
-
--}
